70496fd4792da2042b6dab5b369e1f4176009094
rishavchanda <rishavchanda0@gmail.com>
2024-03-23 21:01:37 +0530

    Added Api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://fitnesstrack-vtv1.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
